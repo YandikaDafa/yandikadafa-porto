02e1a03b8d13bef20fceff9e84f6f63492394c4e
Dafa <143941273+YandikaDafa@users.noreply.github.com>
2026-07-01 21:38:50 +0800

    Refactor Navigation component structure and styles

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -35,18 +35,13 @@
   ></v-btn>
 
   <v-btn 
+  icon="fab fa-tiktok"
   size="large"
   variant="text"
   density="compact"
   href="https://www.tiktok.com/@dafff.999"
   target="_blank"
->
-  <v-icon>
-    <svg viewBox="0 0 24 24" width="24" height="24">
-      <path fill="currentColor" d="M19.59 6.69a4.83 4.83 0 0 1-3.77-4.25V2h-3.45v13.67a2.89 2.89 0 1 1-5.36-1.54 2.89 2.89 0 0 1 4.54-1.28V9.12A6.33 6.33 0 0 0 12 17.5a6.33 6.33 0 0 0 6.33-6.33V6.69z"/>
-    </svg>
-  </v-icon>
-</v-btn>
+></v-btn>
 </div>
     </template>
   </v-app-bar>
